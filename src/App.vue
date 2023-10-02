26553c5d055cc96b0ca17e13bda2fb04db7a38af
Jérémy VIGNELLES <jeremy.vignelles@inetum.com>
2023-10-02 15:41:33 +0200

    fix: Simplification du code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <router-view />
+  <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-title>
+        <v-icon icon="mdi-circle-slice-4" />
+
+        Base Preset
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
